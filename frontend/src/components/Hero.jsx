0bc0a159e6a51f7e4d8d3e7eedad8aabf2a68cd2
anas knene <anas.knene@hotmail.com>
2022-06-28 15:19:30 +0200

    clean code

--- frontend/src/components/Hero.jsx
+++ frontend/src/components/Hero.jsx
@@ -112,7 +112,19 @@ function Hero() {
   return (
     <div>
       {/* <PhotoAlbum photos={photos} layout="masonry" key={photos.id} /> */}
-      <h1>hhhhhhhhhhhhhhhhhhhhhhhhhhhhh</h1>
+      {/* <h1>hhhhhhhhhhhhhhhhhhhhhhhhhhhhh</h1> */}
+      <div className="quote">
+        <p>
+          And I said this, “I fuckin’ hate being poor.” And my <br /> dad got
+          really upset. He didn’t scream or howl. That <br /> wasn’t his way. He
+          just threw his newspaper on the <br /> floor, and he said, “David,
+          David, David. You are not <br /> poor.” He said, “Poor is a
+          mentality.” He said, “It’s a <br /> mentality that very few people
+          ever recover from.
+          <br />
+          Don’t you forget it, son. You are broke.”
+        </p>
+      </div>
     </div>
   );
 }
